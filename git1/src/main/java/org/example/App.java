f44d174586071844ae0d1aeb089dd7de6bbc176d
zqc <1820901097@qq.com>
2022-07-05 20:48:36 +0800

    third commit

--- git1/src/main/java/org/example/App.java
+++ git1/src/main/java/org/example/App.java
@@ -4,10 +4,10 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "third commit!" );
     }
 }
